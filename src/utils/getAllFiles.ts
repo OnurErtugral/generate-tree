5d9e477c3af8c81457cd17d29595086e7679352b
Onur Ertugral <onurertugral2@gmail.com>
2020-04-27 17:30:44 +0300

    feat: exclude node_modules .git from file map & add github corner badge

--- src/utils/getAllFiles.ts
+++ src/utils/getAllFiles.ts
@@ -7,7 +7,15 @@ export default async function getFiles(dir: string) {
   const files = await Promise.all(
     dirents.map((dirent: Dirent) => {
       const res = path.resolve(dir, dirent.name);
-      return dirent.isDirectory() ? getFiles(res) : res;
+      if (!["node_modules", ".git", "dist"].includes(dirent.name)) {
+        if (dirent.isDirectory()) {
+          return getFiles(res);
+        } else {
+          return res;
+        }
+      } else {
+        return "";
+      }
     })
   );
   return Array.prototype.concat(...files);
